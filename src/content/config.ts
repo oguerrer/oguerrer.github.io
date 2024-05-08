5effed5a73e2b281a16908d897d5f0f18feaab3b
Omar Guerrero <oguerrer@gmail.com>
2024-05-08 09:28:29 +0100

    blurb in categories

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const categories = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
+    blurb: z.string(),
     description: z.string(),
     research: z.array(reference('research')),
     cover: z.string().optional(),
